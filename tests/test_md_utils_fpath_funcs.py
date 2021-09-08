11af4f82b9c863343af7af90ccf6346b2dd87281
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-08 21:40:14 +0100

    Initial mocks

---
+++ tests/test_md_utils_fpath_funcs.py
@@ -0,0 +1,27 @@
+import pytest
+
+from obsidian_tools.md_utils import (_get_html_from_md_file,
+                                     _get_plaintext_from_md_file)
+
+
+# tmp_path
+
+@pytest.fixture
+def mocker_md_file(mocker):
+    mocked_output = mocker.mock_open()
+    mocker.patch('builtins.open', mocked_output)
+    return mocked_output
+
+
+def test_get_html_from_md_file(mocker_md_file):
+    # test fake file open returns str
+    actual_html = _get_html_from_md_file(mocker_md_file)
+
+    assert isinstance(actual_html, str)
+
+
+def test_get_plaintext_from_md_file(mocker_md_file):
+    # test fake file open returns str
+    actual_txt = _get_plaintext_from_md_file(mocker_md_file)
+
+    assert isinstance(actual_txt, str)
