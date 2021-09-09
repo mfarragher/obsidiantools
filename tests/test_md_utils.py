9da5e18b00b7ec460220e6f6070882b364c22dea
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-09 23:10:02 +0100

    Markdown link func tests

--- tests/test_md_utils.py
+++ tests/test_md_utils.py
@@ -3,12 +3,13 @@ import pytest
 
 from obsidian_tools.md_utils import (_get_all_wiki_links_from_html_content,
                                      _get_unique_wiki_links,
-                                     _get_ascii_plaintext_from_html)
+                                     _get_all_md_link_info_from_ascii_plaintext,
+                                     _get_unique_md_links_from_ascii_plaintext)
 
 
 @pytest.fixture
 def html_stub():
-    text = r"""
+    html = r"""
     <pre><code># Intro
     This is a very basic string representation.
 
@@ -20,6 +21,26 @@ def html_stub():
 
     Oh and did I say [[Bananas | BANANAS]]??
     There's no link for [Cherries].  Though there is for [[Durians]].
+    """
+    return html
+
+
+@pytest.fixture
+def txt_stub():
+    text = r"""
+    * [The Times 03/Jan/2009 Chancellor on brink of second bailout for banks](<https://www.thetimes.co.uk/article/chancellor-alistair-darling-on-brink-of-second-bailout-for-banks-n9l382mn62h>)
+    * [Chancellor Alistair Darling on brink of second bailout for banks](<https://www.thetimes.co.uk/article/chancellor-alistair-darling-on-brink-of-second-bailout-for-banks-n9l382mn62h>)
+    * [This is a statement inside square brackets]
+    * (This is a statement inside parentheses)
+    * (<https://www.markdownguide.org/basic-syntax/>)[Getting the bracket types in wrong order]
+    * [Markdown basic syntax - <> not in the link](https://www.markdownguide.org/basic-syntax/)
+    * []()
+    * [()]
+    * ([])
+    * [([)
+    * ([)]
+
+    [ADA](<https://cardano.org/>)
     """
     return text
 
@@ -59,3 +80,28 @@ def test_get_unique_wiki_links_from_html_content(html_stub):
 
     assert set(actual_results) == set(expected_results)
     assert isinstance(expected_results, list)
+
+
+def test_get_all_md_link_info(txt_stub):
+    expected_links = [('The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',
+                       'https://www.thetimes.co.uk/article/chancellor-alistair-darling-on-brink-of-second-bailout-for-banks-n9l382mn62h'),
+                      ("Chancellor Alistair Darling on brink of second bailout for banks",
+                      'https://www.thetimes.co.uk/article/chancellor-alistair-darling-on-brink-of-second-bailout-for-banks-n9l382mn62h'),
+                      ('ADA', 'https://cardano.org/')
+                      ]
+    actual_links = _get_all_md_link_info_from_ascii_plaintext(txt_stub)
+
+    assert actual_links == expected_links
+
+
+def test_get_unique_md_links_has_order_preserved(txt_stub):
+    expected_links = ['https://www.thetimes.co.uk/article/chancellor-alistair-darling-on-brink-of-second-bailout-for-banks-n9l382mn62h',
+                      'https://cardano.org/']
+    actual_links = _get_unique_md_links_from_ascii_plaintext(txt_stub)
+
+    assert actual_links == expected_links
+
+
+def test_get_unique_md_links_has_unique_links(txt_stub):
+    actual_links = _get_unique_md_links_from_ascii_plaintext(txt_stub)
+    assert len(set(actual_links)) == len(actual_links)
