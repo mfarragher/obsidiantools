f0700d374db5dce1a13a6c85e092b2a3aa4be994
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-09 22:11:05 +0100

    html2text - better plaintext

--- obsidian_tools/md_utils.py
+++ obsidian_tools/md_utils.py
@@ -27,6 +27,8 @@ def get_wiki_links(filepath):
     """Get wiki links (unique) from a md file.  This accounts for aliases,
     so the [[Lorem ipsum | L.I.]] will be represented as 'Lorem ipsum'.
 
+    Order of appearance in the file is not preserved in the output.
+
     Args:
         filepath (pathlib Path): Path object representing the file from
             which info will be extracted.
@@ -34,26 +36,44 @@ def get_wiki_links(filepath):
     Returns:
         list
     """
-    html_str = _get_plaintext_from_md_file(filepath)
+    plaintext = _get_ascii_plaintext_from_md_file(filepath)
 
-    wikilinks = _get_unique_wiki_links(html_str, remove_aliases=True)
+    wikilinks = _get_unique_wiki_links(plaintext, remove_aliases=True)
     return wikilinks
 
 
 def _get_html_from_md_file(filepath):
+    """md -> html, via markdown lib."""
     with open(filepath) as f:
-        html = markdown.markdown(f.read())
+        html = markdown.markdown(f.read(), output_format='html')
     return html
 
 
-def _get_plaintext_from_html(html):
-    string = html2text.html2text(html)
-    return string
+def _get_html2text_obj_with_config():
+    """Get HTML2Text object with config set."""
+    txt_maker = html2text.HTML2Text()
+
+    # some settings to avoid newline problems with links
+    txt_maker.ignore_links = False
+    txt_maker.body_width = 0
+    txt_maker.protect_links = True
+    txt_maker.wrap_links = False
+    # remove md formatting chars
+    txt_maker.ignore_emphasis = True
+    return txt_maker
 
 
-def _get_plaintext_from_md_file(filepath):
+def _get_ascii_plaintext_from_html(html):
+    """html -> ASCII plaintext, via HTML2Text."""
+    txt_maker = _get_html2text_obj_with_config()
+    doc = txt_maker.handle(html)
+    return doc
+
+
+def _get_ascii_plaintext_from_md_file(filepath):
+    """md file -> html -> ASCII plaintext"""
     html = _get_html_from_md_file(filepath)
-    return _get_plaintext_from_html(html)
+    return _get_ascii_plaintext_from_html(html)
 
 
 def _get_all_wiki_links_from_html_content(html_str, *, remove_aliases=True):
