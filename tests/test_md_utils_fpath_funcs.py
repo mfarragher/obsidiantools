11728f7ca6b807c99582d9c9ef60740b0dbb113f
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-09 23:36:38 +0100

    Mock link getters

--- tests/test_md_utils_fpath_funcs.py
+++ tests/test_md_utils_fpath_funcs.py
@@ -2,7 +2,8 @@ import pytest
 
 from obsidian_tools.md_utils import (_get_html_from_md_file,
                                      _get_ascii_plaintext_from_md_file)
-
+from obsidian_tools.md_utils import (get_md_links,
+                                     get_wiki_links)
 
 # tmp_path
 
@@ -25,3 +26,17 @@ def test_get_plaintext_from_md_file(mocker_md_file):
     actual_txt = _get_ascii_plaintext_from_md_file(mocker_md_file)
 
     assert isinstance(actual_txt, str)
+
+
+def test_get_md_links(mocker_md_file):
+    # test fake file open returns list
+
+    actual_links = get_md_links(mocker_md_file)
+    assert isinstance(actual_links, list)
+
+
+def test_get_wiki_links(mocker_md_file):
+    # test fake file open returns list
+
+    actual_links = get_wiki_links(mocker_md_file)
+    assert isinstance(actual_links, list)
