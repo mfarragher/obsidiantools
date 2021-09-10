3fbd5c8a03acaa76c6d0f6843eefc74b7fe2fe5d
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-10 13:58:05 +0100

    Mock vault getters

--- tests/test_api.py
+++ tests/test_api.py
@@ -25,3 +25,23 @@ def test_vault_initialisation(tmp_path):
 def test_vault_initialisation_needs_directory():
     with pytest.raises(TypeError):
         Vault()
+
+
+def test_get_md_relpaths(mock_initial_vault):
+    mock_output = mock_initial_vault._get_md_relpaths()
+    assert isinstance(mock_output, list)
+
+
+def test_get_md_relpaths_by_name(mock_initial_vault):
+    mock_output = mock_initial_vault._get_md_relpaths_by_name()
+    assert isinstance(mock_output, dict)
+
+
+def test_get_wikilinks_by_md_filename(mock_initial_vault):
+    mock_output = mock_initial_vault._get_wikilinks_by_md_filename()
+    assert isinstance(mock_output, dict)
+
+
+def test_get_unique_wikilinks_by_md_filename(mock_initial_vault):
+    mock_output = mock_initial_vault._get_unique_wikilinks_by_md_filename()
+    assert isinstance(mock_output, dict)
