4f70ca0862831868c29ffcdc4974fd0ffe999733
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-10 15:03:22 +0100

    Connect & graph functionality

--- obsidian_tools/api.py
+++ obsidian_tools/api.py
@@ -1,3 +1,6 @@
+import networkx as nx
+
+
 from .md_utils import (get_md_relpaths_from_dir, get_unique_wikilinks,
                        get_wikilinks)
 
@@ -9,6 +12,18 @@ class Vault:
         instantiate the class.  This class is intended to support multiple
         operating systems so pass a pathlib Path object.
 
+        By calling CONNECT you generate a graph of your vault.
+        All the wikilinks in the vault are used to produce the graph, to
+        reflect how analytics are displayed in Obsidian, where links are
+        counted rather than notes.  For example, if a note has 2 backlinks
+        then there will be 2 wikilinks (to another note) visible when you do
+        analysis of the edges & nodes.
+
+        The API is fluent so you can set up your vault and connect your
+        notes in one line:
+
+        vault = Vault(dirpath).connect()
+
         The class supports subdirectories and relies heavily on relative
         paths for the API.
 
@@ -18,11 +33,21 @@ class Vault:
                 subdirectory of the vault with notes you want to inspect,
                 then you could pass that.
 
+        Methods:
+            connect
+
         Attributes:
-            dirpath
+            dirpath (arg)
+            file_index
+            graph
+            is_connected
         """
         self._dirpath = dirpath
         self._file_index = self._get_md_relpaths_by_name()
+
+        # graph setup
+        self._graph = None
+        self._is_connected = False
 
     @property
     def dirpath(self):
@@ -33,6 +58,35 @@ class Vault:
     def file_index(self):
         """dict: one-to-one mapping of filename (k) to relative path (v)"""
         return self._file_index
+
+    @property
+    def graph(self):
+        """networkx Graph"""
+        return self._graph
+
+    @property
+    def is_connected(self):
+        """Bool: has the connect function been called to set up graph?"""
+        return self._is_connected
+
+    def connect(self):
+        """connect your notes together by representing the vault as a
+        Networkx graph object, G.
+
+        With your vault instantiated, set up the graph through:
+            vault.connect()
+
+        The graph G is written to the 'graph' attribute.
+        """
+        if not self._is_connected:
+            # default graph to mirror Obsidian's link counts
+            wiki_link_map = self._get_wikilinks_by_md_filename()
+            G = nx.MultiDiGraph(wiki_link_map)
+            self._graph = G
+
+            self._is_connected = True
+
+        return self  # fluent
 
     def _get_md_relpaths(self):
         """Return list of filepaths *relative* to the directory instantiated
