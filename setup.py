9133a531a8a0e68b259712889417a40835666f59
mfarragher <33595938+mfarragher@users.noreply.github.com>
2021-09-11 22:56:22 +0100

    Setup initial

---
+++ setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as f:
+    LONG_DESCRIPTION = f.read()
+
+PROJECT_URLS = {"Source": "https://github.com/mfarragher/obsidian_tools"}
+INSTALL_REQUIRES = [
+    "markdown",
+    "html2text",
+    "pandas",
+    "numpy",
+    "networkx"]
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "Topic :: Text Processing :: Markup :: Markdown",
+    "Topic :: Office/Business",
+    "Topic :: Education",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.7",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+]
+
+setuptools.setup(
+    name="obsidian_tools",
+    version="0.5.0",
+    author="Mark Farragher",
+    description="Obsidian Tools - a Python interface for Obsidian.md vaults",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords=["obsidian", "obsidian.md", "knowledge",
+              "note-taking", "notes",
+              "knowledge management",
+              "connected notes"],
+    url="https://github.com/mfarragher/obsidian_tools",
+    project_urls=PROJECT_URLS,
+    packages=setuptools.find_packages(exclude=("tests")),
+    python_requires=">=3.7",
+    install_requires=INSTALL_REQUIRES,
+    license="BSD",
+    classifiers=CLASSIFIERS
+)
