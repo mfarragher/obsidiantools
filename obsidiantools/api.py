f71f05427d9c78660554ca3c3c3156bdaf04bea9
jcapelle13 <14522128+jcapelle13@users.noreply.github.com>
2026-01-11 15:18:32 -0500

    Fix Vault.get_front_matter type hint

--- obsidiantools/api.py
+++ obsidiantools/api.py
@@ -960,7 +960,7 @@ class Vault:
         else:
             return self._md_links_index[file_name]
 
-    def get_front_matter(self, file_name: str) -> list[dict]:
+    def get_front_matter(self, file_name: str) -> dict:
         """Get front matter for a note (given its filename).
 
         Front matter can only appear in notes that already exist, so if a
@@ -972,7 +972,7 @@ class Vault:
                 This is NOT the filepath!
 
         Returns:
-            list
+            dict
         """
         if not self._is_connected:
             raise AttributeError('Connect notes before calling the function')
